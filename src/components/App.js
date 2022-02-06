1fa57cef0ebac6d75e6ecc9bfb31fa1d8fb5385d
rdelva <roseldelva@gmail.com>
2022-02-05 21:34:00 -0500

    updated enableSearch

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     this.enableSearch();
   }
 
-  enableSearch = () =>{
+  enableSearch = (query = 'cats') =>{
 
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=3d92e860f2bbda59f5984f034285876f&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
