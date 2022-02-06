7d6578fec588b5360cff2ea48fbe9fd6d4759ac8
rdelva <roseldelva@gmail.com>
2022-02-05 21:31:49 -0500

    added properties to form

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,18 @@ class App extends Component {
 
   enableSearch = () =>{
 
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=3d92e860f2bbda59f5984f034285876f&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+      this.setState({
+        gifs: response.data.data,
+        loading: false
+      });
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+
+
   }
 
   render(){
