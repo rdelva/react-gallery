02f9175884fe135e40bcd5ef9b1be92274e4b3ac
rdelva <roseldelva@gmail.com>
2022-02-12 00:22:53 -0500

    updated config gile and changed import name to apiKey

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchForm from './SearchForm';
 import Nav from './Nav';
 import Photo from './Photo';
 import axios from 'axios';
-import config from './config';
+import apiKey from '../config';
 
 class App extends Component {
   constructor(){
@@ -20,7 +20,7 @@ class App extends Component {
   }
 
   enableSearch = (query = 'cats') =>{
-
+ 
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
       this.setState({
