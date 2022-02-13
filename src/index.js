fdbb395417f1f3a130b44e5d451785ec4e9256a1
rdelva <roseldelva@gmail.com>
2022-02-12 20:26:49 -0500

    updated the import link for stylesheet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/style.css';
 import './css/index.css';
-import './css/style.css'
+
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root')
