4c1afb2171071ae65b8deb8624ec6f074b33100f
rdelva <roseldelva@gmail.com>
2022-02-20 00:39:17 -0500

    moved code to searchform and updated browser router routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,38 @@
-import React, { Component} from 'react';
+import React from 'react';
 import {
   BrowserRouter,
   Route,
   Switch
 } from 'react-router-dom';
+
+//import Components
 import SearchForm from './SearchForm';
 import Nav from './Nav';
 import Photos from './Photos';
-import axios from 'axios';
-import apiKey from '../config';
+import NotFound from './NotFound';
 
 
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      gifs:[],
-      loading: true
-    }
-  }
+const App = () => {
+
+  return(
+    <BrowserRouter> 
+        <SearchForm />
+        <Nav />
+        <Switch>
+          <Route exact path="/" render ={() => <Photos /> } />
+          <Route path="/cats" render={()=> <Photos />} />
+          <Route path="/shoes" render={()=> <Photos />} />
+          <Route path="/flowers" render={()=> <Photos />} />          
+          <Route component={<NotFound/>} />
+        </Switch>
+       
+   </BrowserRouter>
 
 
-  componentDidMount(){
-    this.enableSearch();
-  }
+  );
 
-  enableSearch = (query = 'sunrise') =>{
- 
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-    .then(response => {
-      this.setState({
-        gifs: response.data.photos.photo,
-        loading: false
-      });
-  
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
-
-
-  }
-
-  render(){
-    return (
-      <BrowserRouter>
-        <div className="container">          
-            <Route path ="/" component={SearchForm} />
-            <Route path = "/" component={Nav} />
-            <div className="main-content">
-              {
-                (this.state.loading) 
-                  ? <p>Loading ...</p>
-                  : <Route path ="/" render={ () => <Photos data = {this.state.gifs}/>} />                
-              }
-            </div>
-        
-        </div> 
-      </BrowserRouter>
-    );
-  }
-
-  }
+}
 
 
 export default App;
