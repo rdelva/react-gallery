4ef9561047ae2628bdcbbf25cbe2f8317fddd92c
rdelva <roseldelva@gmail.com>
2022-02-20 11:37:20 -0500

    changed App from component to class and axios for search query

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import apiKey from '../config';
+
 import {
   BrowserRouter,
   Route,
@@ -13,23 +16,57 @@ import NotFound from './NotFound';
 
 
 
-const App = () => (
-  <BrowserRouter> 
-    <SearchForm />
-    <Nav />
-    <Switch>
-    <Route exact path="/" />
-      <Route path="/cats" component={Photos} />
-      <Route path="/shoes" component={Photos} />
-      <Route path="/flowers" component={Photos} />          
-      <Route component={<NotFound/>} />
-    </Switch>
- 
-</BrowserRouter>
-);
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        gifs:[]
+    };
+  }
+
+  componentDidMount(){
+    this.performSearch();
+  }
+
+  performSearch = (query = 'cats') => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then( response => {
+            
+            this.setState({
+              gifs:response.data.photos.photo,
+              loading:false
+            });
+    })
+    .catch(
+       error => {
+         console.log('Error fetch and parsing data', error);
+       });
+  }
+
+  render(){
+    
+    return (
+      <BrowserRouter> 
+      <SearchForm onSearch={this.performSearch} />
+      <Nav />
+        {
+          (this.state.loading)
+          ? <p>Loading...</p>
+          : <Photos data={this.state.gifs} />          
+        }
+
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/cats" component={Photos} />
+          <Route path="/shoes" component={Photos} />
+          <Route path="/flowers" component={Photos} />          
+          <Route component={NotFound} />
+        </Switch> 
+      </BrowserRouter>
+    );
+  }
+}
    
 
 
-
-
-export default App;
+//export default App;
