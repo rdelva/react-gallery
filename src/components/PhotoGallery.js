c66ca7daacbf3e92b4dfe3c20d0927eeea2054c2
rdelva <roseldelva@gmail.com>
2022-03-06 16:50:01 -0500

    setting up gallery

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import NotFound from './NotFound';
 import Photo from './Photo';
  
 const PhotoGallery = (props) =>{
-    console.log(props);
+    const photoList = props.data;
+    let images;
+
+    if(photoList.length > 0 ){
+        images = photoList.map((photo, index) => 
+            <Photo server={photoList.server} secret={photoList.secret} id={photoList.id} key={index}  />           
+        );
+    } else {
+        images = <NotFound />
+    }
+    
+  
+
     return(
-        <div className="">
+        <div className="photo-container">
             <ul>
+                {
+                    {images}
+                }
                 
             </ul>
         </div>
