f69fdb458310c8d04e3c369b6b1d60f14d1abb86
rdelva <roseldelva@gmail.com>
2022-03-16 11:42:55 -0400

    moved the history.push to above the props.onSearch

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,9 +18,10 @@ class SearchForm extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
+        this.props.history.push(this.query.value);
+
         this.props.onSearch(this.query.value);
         e.currentTarget.reset();
-        this.props.history.push(this.query.value);
         
     }
 
